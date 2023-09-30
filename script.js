e8718d6a04883851b6cfbb79bfa8e414cad38ae8
Wadood342 <mianwadood342@gmail.com>
2023-10-01 04:39:43 +0500

    styling, Edit and local storage Functionality

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const addBtn = document.querySelector("#addBtn");
 const mainInput = document.querySelector("#mainInput");
-const toDoList =  document.querySelector("#toDoList");
 const tableBody = document.querySelector("#tableBody");
 const validator = document.querySelector("#validator");
 const clearAll = document.querySelector (".ClearAll-icon")
-let srNo = 0;
+const clearMessage = document.querySelector("#clearMessage");
+const toDoTable = document.querySelector("#toDoTable")
 
-
+function saveAll(){
+const allTodos =JSON.stringify(toDoTable.innerHTML);
+localStorage.setItem("MyToDos", allTodos);
+}
 
 function addTodo() {
-    if(mainInput.value !== ""){
-        srNo++;
+    if(mainInput.value.trim() !== ""  ){
+    
     const list = document.createElement("tr");
-    list.innerHTML= `<td>${srNo}.</td> <td>${mainInput.value}</td> <td><button class="btn btn-danger mx-1 del-btn"><img src="assets/delete.svg" alt="" class="" onclick="removeToDo(this)"></button></td>`; 
+    list.innerHTML=  `<td>${mainInput.value}</td>
+    <td>
+        <button class="btn btn-success mx-1 edit-btn" onclick="editTask(this)">
+            <img src="assets/edit.svg" alt="Edit" class="edit-icon">
+        </button>
+        <button class="btn btn-danger mx-1 del-btn rem-btn">
+            <img src="assets/delete.svg" alt="Remove" class="" onclick="removeToDo(this)">
+        </button>
+    </td>`; 
     tableBody.appendChild(list);
     mainInput.value="";
     mainInput.classList.remove("redBorder");
@@ -22,14 +33,51 @@ else{
     mainInput.classList.add("redBorder");
     validator.innerHTML= `<h5 class="validator">Please add task</h5>`;
 }
+clearMessage.style.display = "none";
+saveAll();
 }
+
 
 function removeToDo(button){
     const row = button.closest("tr");
     row.remove();
+    saveAll();
+ }
+function clearAllTodos() {
+    if (tableBody.children.length === 0) {
+        clearMessage.style.display = "block";
+        mainInput.classList.add("redBorder");
+    } else {
+
+        tableBody.innerHTML = "";
+        clearMessage.style.display = "none";
+        mainInput.classList.remove("redBorder");
+        saveAll();
+     }
 }
-function clearAllTodos(){
-   
-    tableBody.innerHTML="";
-    srNo = 0;
+function editTask(button) {
+    const row = button.closest("tr");
+    const taskCell = row.querySelector("td:first-child");
+    const editBtn = row.querySelector(".edit-icon");
+
+    if (taskCell.contentEditable === "false") {
+    
+        editBtn.src = "assets/done.svg"; 
+        taskCell.contentEditable = true;
+        taskCell.focus();
+    } else {
+        
+        editBtn.src = "assets/edit.svg"; 
+        taskCell.contentEditable = false;
+        saveAll();    }
 }
+
+function loadToDos() {
+    const allTodos = JSON.parse(localStorage.getItem("MyToDos"));
+    if (allTodos) {
+        tableBody.innerHTML = allTodos;
+    }
+}
+
+
+loadToDos();
