5e96cfa9c4d347dc433b2d6a3eb5b7523a386631
Wadood342 <mianwadood342@gmail.com>
2023-10-02 17:44:29 +0500

    Placeholder and gave validator to Edit Button

--- script.js
+++ script.js
@@ -2,47 +2,48 @@ const addBtn = document.querySelector("#addBtn");
 const mainInput = document.querySelector("#mainInput");
 const tableBody = document.querySelector("#tableBody");
 const validator = document.querySelector("#validator");
-const clearAll = document.querySelector (".ClearAll-icon")
+const clearAll = document.querySelector(".ClearAll-icon")
 const clearMessage = document.querySelector("#clearMessage");
 const toDoTable = document.querySelector("#toDoTable")
-
-function saveAll(){
-const allTodos =JSON.stringify(toDoTable.innerHTML);
-localStorage.setItem("MyToDos", allTodos);
+let srNo = 0;
+function saveAll() {
+    const allTodos = JSON.stringify(toDoTable.innerHTML);
+    localStorage.setItem("MyToDos", allTodos);
 }
 
 function addTodo() {
-    if(mainInput.value.trim() !== ""  ){
-    
-    const list = document.createElement("tr");
-    list.innerHTML=  `<td>${mainInput.value}</td>
+    if (mainInput.value.trim() !== "") {
+        srNo++;
+        const list = document.createElement("tr");
+        list.innerHTML = `<td>${mainInput.value}</td>
     <td>
+    <button class="btn btn-danger mx-1 del-btn rem-btn">
+    <img src="assets/delete.svg" alt="Remove" class="" onclick="removeToDo(this)">
+</button>
         <button class="btn btn-success mx-1 edit-btn" onclick="editTask(this)">
             <img src="assets/edit.svg" alt="Edit" class="edit-icon">
         </button>
-        <button class="btn btn-danger mx-1 del-btn rem-btn">
-            <img src="assets/delete.svg" alt="Remove" class="" onclick="removeToDo(this)">
-        </button>
-    </td>`; 
-    tableBody.appendChild(list);
-    mainInput.value="";
-    mainInput.classList.remove("redBorder");
-    validator.textContent=``
-}
-else{
-    mainInput.classList.add("redBorder");
-    validator.innerHTML= `<h5 class="validator">Please add task</h5>`;
-}
-clearMessage.style.display = "none";
-saveAll();
+       
+    </td>`;
+        tableBody.appendChild(list);
+        mainInput.value = "";
+        mainInput.classList.remove("redBorder");
+        validator.textContent = ``
+    }
+    else {
+        mainInput.classList.add("redBorder");
+        validator.innerHTML = `<h5 class="validator">Please add task</h5>`;
+    }
+    clearMessage.style.display = "none";
+    saveAll();
 }
 
 
-function removeToDo(button){
+function removeToDo(button) {
     const row = button.closest("tr");
     row.remove();
     saveAll();
- }
+}
 function clearAllTodos() {
     if (tableBody.children.length === 0) {
         clearMessage.style.display = "block";
@@ -53,7 +54,7 @@ function clearAllTodos() {
         clearMessage.style.display = "none";
         mainInput.classList.remove("redBorder");
         saveAll();
-     }
+    }
 }
 function editTask(button) {
     const row = button.closest("tr");
@@ -61,15 +62,30 @@ function editTask(button) {
     const editBtn = row.querySelector(".edit-icon");
 
     if (taskCell.contentEditable === "false") {
-    
-        editBtn.src = "assets/done.svg"; 
+        taskCell.classList.add("redBorder"); 
+        editBtn.src = "assets/done.svg";
         taskCell.contentEditable = true;
-        taskCell.focus();
-    } else {
         
-        editBtn.src = "assets/edit.svg"; 
-        taskCell.contentEditable = false;
-        saveAll();    }
+        taskCell.focus();
+        
+    } else {
+
+        if (taskCell.textContent.trim() == "") {
+            taskCell.classList.add("redBorder"); 
+            validator.innerHTML = `<h5 class=validator>Please add Task</h5>`;
+            taskCell.focus();
+            
+        }
+        else {
+            validator.textContent = ``
+            editBtn.src = "assets/edit.svg";
+            taskCell.contentEditable = false;
+            taskCell.classList.remove("redBorder"); 
+            
+        }
+    }
+    saveAll();
+
 }
 
 function loadToDos() {
